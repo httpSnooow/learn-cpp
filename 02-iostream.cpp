2d81fb526cc66de7155234c5d8b632ae0e8982ab
Guilherme Eduardo <gededeus@gmail.com>
2026-01-27 21:09:13 -0300

    Update 02-iostream.cpp

--- 02-iostream.cpp
+++ 02-iostream.cpp
@@ -3,7 +3,7 @@
 int main () {
 	int x{}, y{};
 	
-	std::cout << "Enter a number: " << std::endl;
+	std::cout << "Enter two numbers: " << std::endl;
 	std::cin >> x >> y;
 
 	std::cout << "You entered " << x << " and " << y << ".\n";
